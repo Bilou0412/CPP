e1775bd18caf65fc5685dde87f1cb0a7ba34027d
Bilel <bilel.moudache@proton.me>
2024-04-11 18:12:18 +0200

    finish

--- cpp0/ex01/cpp/PhoneBook.class.cpp
+++ cpp0/ex01/cpp/PhoneBook.class.cpp
@@ -13,28 +13,53 @@ void    PhoneBook::add(void)
         this->index = 0;
 
     std::string PhoneNb;
-    std::cout << "Phone Number :";
-    std::getline(std::cin,PhoneNb);
+    while(PhoneNb.empty() && !std::cin.eof())
+    {
+        std::cout << "Phone Number :";
+        std::getline(std::cin,PhoneNb);
+        if(PhoneNb.empty() && !std::cin.eof())
+            std::cout << "Cannot be empty" <<std::endl;
+    }
     this->Contact_list[this->index].SetPhoneNumber(PhoneNb);
 
     std::string FirstName;
-    std::cout << "First Name :" ;
-    std::getline (std::cin,FirstName);
+    while(FirstName.empty() && !std::cin.eof())
+    {
+        std::cout << "First Name :";
+        std::getline(std::cin,FirstName);
+        if(FirstName.empty() && !std::cin.eof())
+            std::cout << "Cannot be empty" <<std::endl;
+    }
     this->Contact_list[this->index].SetFirstName(FirstName);
 
     std::string LastName;
-    std::cout << "Last Name :";
-    std::getline(std::cin,LastName);
+    while(LastName.empty() && !std::cin.eof())
+    {
+        std::cout << "Last Name :";
+        std::getline(std::cin,LastName);
+        if(LastName.empty() && !std::cin.eof())
+            std::cout<< "Cannot be empty" << std::endl;
+    }
     this->Contact_list[this->index].SetLastName(LastName);
 
     std::string NickName;
-    std::cout << "NickName :";
-    std::getline(std::cin,NickName);
+    while(NickName.empty() && !std::cin.eof())
+    {
+        std::cout << "NickName :";
+        std::getline(std::cin,NickName);
+        if(NickName.empty() && !std::cin.eof())
+            std::cout << "Cannot be empty" << std::endl;
+    }
     this->Contact_list[this->index].SetNickName(NickName);
 
     std::string DarkestSecret;
-    std::cout << "Darkest Secret :";
-    std::getline(std::cin,DarkestSecret);
+    while(DarkestSecret.empty() && !std::cin.eof())
+    {
+        std::cout << "Darkest Secret :";
+        std::getline(std::cin,DarkestSecret);
+        if(DarkestSecret.empty() && !std::cin.eof())
+            std::cout << "Cannot be empty" << std::endl;
+    }
     this->Contact_list[this->index].SetDarkestSecret(DarkestSecret);
 
     this->index++;
@@ -61,9 +86,11 @@ void    PhoneBook::search()
         if(index.size() != 1 || (index[0] < '0' || index[0] > '7'))
             std::cout << "Please Enter Number between 0 and 7 inclusive" <<std::endl;
     }
-    std::cout<<"_____________________________________________"<<std::endl;
-    std::cout<<DisplayInfo(index[0] - '0')<<std::endl;
-    std::cout<<"‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾"<<std::endl;
+    std::cout << "First Name     : " << Contact_list[index[0] - '0'].GetFirstName() << std::endl;
+    std::cout << "Last Name      : " << Contact_list[index[0] - '0'].GetLastName() << std::endl;
+    std::cout << "NickName       : " << Contact_list[index[0] - '0'].GetNickName() << std::endl;
+    std::cout << "Phone Number   : " << Contact_list[index[0] - '0'].GetPhoneNumber() << std::endl;
+    std::cout << "Darkest Secret : " << Contact_list[index[0] - '0'].GetDarkestSecret() << std::endl;
 }
 
 std::string PhoneBook::DisplayInfo(int i)
