98140c40827edcb11513269df9c6ffe1ec3277d1
Bilel <bilel.moudache@proton.me>
2024-04-18 14:57:55 +0200

    ok

--- cpp1/ex05/cpp/main.cpp
+++ cpp1/ex05/cpp/main.cpp
@@ -1,10 +1,19 @@
 #include "Harl.class.hpp"
 
-int main(int argc, char **argv)
+/*int main(int argc, char **argv)
 {
   if (argc != 2)
     return (std::cerr << "Usage: Harl [DEBUG || INFO || WARNING || ERROR]" << std::endl, 0);
   Harl harl;
   harl.complain(argv[1]);
   return (0);
+}*/
+
+int main(void)
+{
+  Harl harl;
+  harl.complain("DEBUG");
+  harl.complain("INFO");
+  harl.complain("WARNING");
+  harl.complain("ERROR");
 }
